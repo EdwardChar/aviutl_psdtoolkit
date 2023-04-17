20c4b18189d78dfc9ab06cc6134744ab8aa7495c
bathfire <>
2023-04-17 15:05:18 +0800

    扩展编辑 hex 修改

--- src/c/aviutl.c
+++ src/c/aviutl.c
@@ -55,7 +55,7 @@ cleanup:
 }
 
 NODISCARD static error find_exedit_filter(FILTER const **const exedit_fp, bool *const is_enpatched) {
-  static TCHAR const exedit_name_mbcs[] = "\xE6\x89\xA9\xE5\xB1\x95\xE7\xBC\x96\xE8\xBE\x91"; // "拡張編集"
+  static TCHAR const exedit_name_mbcs[] = "\xc0\xa9\xd5\xb9\xb1\xe0\xbc\xad"; // "扩展编辑"
   static TCHAR const enpatched_exedit_name_mbcs[] = "Advanced Editing";
 
   *exedit_fp = NULL;
